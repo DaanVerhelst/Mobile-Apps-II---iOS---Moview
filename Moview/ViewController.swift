1d6174b7fed9903c12f8242ebc2b837e1a4c07fa
Daan Verhelst <daan.verhelst@student.hogent.be>
2020-08-21 11:23:30 +0200

    Initial Commit

---
+++ Moview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moview
+//
+//  Created by Gebruiker on 21/08/2020.
+//  Copyright © 2020 Gebruiker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
